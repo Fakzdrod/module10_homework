07b698bf0e2cbfa235786203e92c804d3a539ce4
Fakzdrod <Fakzdrod@yandex.ru>
2024-05-20 16:52:07 +0300

    исправил

--- ex1.js
+++ ex1.js
@@ -1,17 +1,12 @@
+let get = prompt("напиши число");
+console.log(get);
 
-let get = prompt();
-let num1 = Number(get);
-let num2 = num1 + Number(3);
-
-console.log(typeof get)
-console.log(typeof num1)
-
-if (typeof get === 'Number') {
-  console.log('Упс, кажется, вы ошиблись')
-} 
-
-if (num2% 2== 0) {
-  console.log('четное')
+if ((typeof(+get) == 'number') && (!isNaN(+get))){
+    if (get % 2 === 0) {
+        console.log("четное");
+    } else {
+        console.log("нечетное");
+    }
 } else {
-  console.log('не четное')
+    console.log("Упс, кажется вы ошиблись");
 }
